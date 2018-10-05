01d3064978063bcaac32e49cb0c5a30a56f1d58e
Ben <ben.challenor@btopenworld.com>
2018-10-05 13:46:50 +0100

    Change image dimentions

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
+
   <section class="container">
   <div>
     <b-carousel id="carousel1"
                 style="text-shadow: 1px 1px 2px #333;"
                 controls
                 indicators
-                background="#ababab"
+                background="black"
                 :interval="1000"
-                img-width="1024"
-                img-height="480">
+                width="auto"
+                >
+
 
       <b-carousel-slide v-for="(image,index) in images":key="image.id" :img-src="image.assets.large.url">
       </b-carousel-slide>
@@ -31,14 +33,14 @@ export default {
     }
   },
 
-  methods: {
-    onSlideStart (slide) {
-      this.sliding = true
-    },
-    onSlideEnd (slide) {
-      this.sliding = false
-    }
-  },
+  // methods: {
+  //   onSlideStart (slide) {
+  //     this.sliding = true
+  //   },
+  //   onSlideEnd (slide) {
+  //     this.sliding = false
+  //   }
+  // },
 
   created() {
     axios.get('http://api.lomography.com/v1/photos/popular?api_key=73023b4ef2870abf40b6786e732625').then(images=>{
@@ -54,13 +56,29 @@ export default {
 </script>
 
 <style>
-.images {
-  column-count: 2;
+.b-carousel {
+/* height: 300px */
+
+}
+
+.w-100 {
+  height: 600px;
+  width: auto !important;
+}
+
+.img-fluid {
+  width: auto;
 }
 
 .container {
   /* margin-left: 0;
   margin-right: 0; */
-  width: 100%;
+  width: auto;
 }
+
+.carousel-item.active {
+align-items: center;
+}
+
+
 </style>
